cd7e9c7610460a04f652c3d3523250e05e1cce5f
Luke Storey <lukemstorey@gmail.com>
2022-06-01 14:30:23 +0100

    Add createNote api function

--- notesApi.js
+++ notesApi.js
@@ -6,6 +6,20 @@ class NotesApi {
         callback(data);
       });
   }
+
+  createNote(note, callback) {
+    fetch("http://localhost:3000/notes", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ content: note }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        callback(data);
+      });
+  }
 }
 
 module.exports = NotesApi;
--- notesApi.test.js
+++ notesApi.test.js
@@ -3,27 +3,45 @@ const NotesApi = require("./notesApi");
 require("jest-fetch-mock").enableFetchMocks();
 
 describe("Api", () => {
-  it("calls fetch and loads data", () => {
-    const api = new NotesApi();
+  describe("loadNotes", () => {
+    it("calls fetch and loads data", (done) => {
+      const api = new NotesApi();
 
-    fetch.mockResponseOnce(
-      JSON.stringify(["This note is coming from the server"])
-    );
+      fetch.mockResponseOnce(
+        JSON.stringify(["This note is coming from the server"])
+      );
 
-    api.loadNotes((returnedNotesData) => {
-      expect(returnedNotesData[0]).toBe("This note is coming from the server");
+      api.loadNotes((returnedNotesData) => {
+        expect(returnedNotesData[0]).toBe(
+          "This note is coming from the server"
+        );
+        expect(fetch.mock.calls[0][0]).toEqual("http://localhost:3000/notes");
+        done();
+      });
     });
   });
 
-  it("uses a callback function", () => {
-    const api = new NotesApi();
+  describe("createNote", () => {
+    it("creates a new note on the server", (done) => {
+      const api = new NotesApi();
 
-    fetch.mockResponseOnce(
-      JSON.stringify(["This note is coming from the server"])
-    );
+      fetch.mockResponse(async (request) => {
+        try {
+          expect(request.method).toBe("POST");
+          const requestBody = await request.json();
+          expect(requestBody.content).toEqual("New note");
+        } catch (err) {
+          done(err);
+        }
 
-    api.loadNotes((callback) => {
-      expect(callback).toBeTruthy();
+        return JSON.stringify(["New note"]);
+      });
+
+      api.createNote("New note", (response) => {
+        expect(response[0]).toBe("New note");
+
+        done();
+      });
     });
   });
 });
